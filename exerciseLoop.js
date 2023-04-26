a7583574b2894fb4c5b4a0e290b620908cd6082f
EvaDM <evamorante1969@gmail.com>
2023-04-26 16:28:25 +0200

    recherche de solution avec if condition pour singulier/pluriel sur step

--- exerciseLoop.js
+++ exerciseLoop.js
@@ -1,8 +1,12 @@
+// exercice 20. module 8
+
 for (var i = 1; i<4; i = i + 1){
-    if (i == 1)
+    if (i == 1){
     console.log(i.toString() + " step");
-}
-for (var i = 1; i>1 && i<4; i = i + 1){
+    }
+    else{
     console.log(i.toString() + " steps");
+    }
+
 }
 console.log("Done !")
