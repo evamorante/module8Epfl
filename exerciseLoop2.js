83ba24ac675de26c673d2dea4c1fa16c1b19a60a
EvaDM <evamorante1969@gmail.com>
2023-04-26 16:33:49 +0200

    new file with second part of exercise about loop and while loop in JS

---
+++ exerciseLoop2.js
@@ -0,0 +1,13 @@
+// exercice 20./part 2 module 8
+
+var i = 1;
+while ( i<4){
+    if (i == 1){
+    console.log(i.toString() + " step");
+    }
+    else{
+    console.log(i.toString() + " steps");
+    }
+
+}
+console.log("Done !")
