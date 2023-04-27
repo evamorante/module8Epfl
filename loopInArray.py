3a208e14ee75313ed0429d20cb470b468ce0827c
EvaDM <evamorante1969@gmail.com>
2023-04-27 14:49:39 +0200

    testLoopArray.py

---
+++ loopInArray.py
@@ -0,0 +1,7 @@
+songs = ["Highway for Heaven", "Viens je t'emmène", "La corrida"]
+for x in range(4):
+    print(songs[0])
+
+for x in range(10):
+    print(songs[0,1])
+# TypeError: list indices must be integers or slices, not tuple
