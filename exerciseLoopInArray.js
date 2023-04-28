3128390492f9490d3349cee4ead7411fcdefb221
EvaDM <evamorante1969@gmail.com>
2023-04-28 16:08:59 +0200

    creating new file for the exercise 26 about looping in arrays in JS

---
+++ exerciseLoopInArray.js
@@ -0,0 +1,14 @@
+/** exercice 26. module 8
+Here are my favorite dishes:
+First dish
+Second dish
+Third dish
+What are your favorite dishes?*/
+
+var dishes = ["fondue", "raclette", "couscous"];
+
+console.log("Here are my favorite dishes:")
+for (i = 0; i < dishes.length; i = i + 1){
+    console.log(dishes[i]);
+}
+console.log("What are yours ?")
