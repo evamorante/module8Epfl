3ddc7300bc40203bede8bdbbcc7b77b7f62dec47
EvaDM <evamorante1969@gmail.com>
2023-04-28 16:39:51 +0200

    new file about dictionnaries in python

---
+++ namedCollections.py
@@ -0,0 +1,6 @@
+# exercice 29. module 8
+my_card = { "name" : "Eva",
+            "currency" : "Dollars",
+            "bank" : "Credit Suisse",
+            "amount" : "126'000'000"}
+print(my_card["currency"])
