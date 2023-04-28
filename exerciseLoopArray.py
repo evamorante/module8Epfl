72dfc6df08360ca62b853449d9c6c018a1430e73
EvaDM <evamorante1969@gmail.com>
2023-04-28 15:49:49 +0200

    exercise about loop in arrays in py

---
+++ exerciseLoopArray.py
@@ -0,0 +1,16 @@
+# exercice 23. module 8
+#These are my favorite songs:
+#First song
+#Second song
+#Third song
+#Fourth song
+#What are yours?
+
+songs = ["Highway for Heaven", 
+         "Viens je t'emmène", 
+         "La corrida"]
+
+print("These are my favorite songs:")
+for song in songs:
+    print(song)
+print("What are yours ?")
