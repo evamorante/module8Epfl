08d924a6d48e9a08e546bb6dbee7a717d06b83a4
EvaDM <evamorante1969@gmail.com>
2023-04-28 16:50:16 +0200

    creating new fils about JS-objects - means named collections

---
+++ JSobjects.js
@@ -0,0 +1,7 @@
+var myCard = { name : "Eva",
+            currency : "Dollars",
+            bank : "Credit Suisse",
+            amount : "126'000'000"}
+console.log(myCard.currency);
+console.log(myCard["amount"]);
+
