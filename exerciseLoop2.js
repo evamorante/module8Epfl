0f763fe9fd47bbf3355a7726b69fa62fe1c25719
EvaDM <evamorante1969@gmail.com>
2023-04-28 14:56:28 +0200

    changes for testing

--- exerciseLoop2.js
+++ exerciseLoop2.js
@@ -16,9 +16,21 @@ console.log("Done !")
 // < que 4 --> donne 4 steps quand même WHY ??
 
 var i = 0;
-i = i + 1;
 while ( i<4) {
-    
+    if (i == 1){
+    console.log(i.toString() + " step");
+    }
+    else{
+    console.log(i.toString() + " steps");
+    }
+    i = i + 1;
+
+}
+console.log("Done !")
+
+var i = 1;
+while ( i<4) {
+    i = i + 1;
     if (i == 1){
     console.log(i.toString() + " step");
     }
