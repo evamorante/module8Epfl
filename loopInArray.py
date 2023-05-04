b09b36b8cf75dc3f43b83686ad9d7cf62335e60f
EvaDM <evamorante1969@gmail.com>
2023-05-04 09:46:13 +0200

    rajout exemple code valide

--- loopInArray.py
+++ loopInArray.py
@@ -6,7 +6,11 @@ for x in range(3):
 # not the best solution, because if adding a data or more, the number of the range has to be changed
 
 for x in songs:
-    print(x) # no more an iterator - so could call it fruit or b or bloubiboulga
-             # the video in the point 22. to be seen again if not understood
-# could add some datas in the array, and it will always work...not with the previous code
-            
+    print(x) # x or i (usual for coders) no more an iterator - so could call it fruit or b or bloubiboulga
+# could add some datas in the array, and it will always work...not with the previous code; with the previous
+# code need to change the number of range(...) 
+# the video in the point 22. to be seen again if not understood      
+
+for song in songs:
+    print(song) # as we could name the variable x as we want, we call it song for example  
+       
